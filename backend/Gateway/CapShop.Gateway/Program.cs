b672e8e619c58b074e112c57a0761ebda5f117f2
Shivam Kumar <kumarshivam16062004@gmail.com>
2026-03-27 13:15:23 +0530

    updating gateway for cors enabling

--- backend/Gateway/CapShop.Gateway/Program.cs
+++ backend/Gateway/CapShop.Gateway/Program.cs
@@ -4,22 +4,31 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
-        .SetBasePath(builder.Environment.ContentRootPath)
-        .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-        .AddEnvironmentVariables();
+.SetBasePath(builder.Environment.ContentRootPath)
+.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+.AddEnvironmentVariables();
 
+builder.Services.AddCors(options =>
+{
+        options.AddPolicy("FrontendPolicy", policy =>
+        {
+                policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+        });
+});
 
-builder.Services.AddOcelot(builder.Configuration);  
-
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseCors("FrontendPolicy");
 
-
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+        app.UseHttpsRedirection();
+}
 
 await app.UseOcelot();
-
-
 
 app.Run();
